49172068c2f7150988d0282c8369c0df20bfd2cd
Purvi <pandyapurvi134@yahoo.in>
2019-08-16 14:24:56 +1000

    Updates

---
+++ precache-manifest.568250d718f1bf715415d5d8b2bd723f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce590d3cc63f394aa91fbbec8c58babb",
+    "url": "/address-diary-react-client/index.html"
+  },
+  {
+    "revision": "68987c49d8a24b387d4a",
+    "url": "/address-diary-react-client/static/css/main.8bf60d2b.chunk.css"
+  },
+  {
+    "revision": "e38d988dd663e42dd964",
+    "url": "/address-diary-react-client/static/js/2.ad9326d7.chunk.js"
+  },
+  {
+    "revision": "68987c49d8a24b387d4a",
+    "url": "/address-diary-react-client/static/js/main.0de788d6.chunk.js"
+  },
+  {
+    "revision": "8879dd1f5aa6bb59cd86",
+    "url": "/address-diary-react-client/static/js/runtime~main.3fc98158.js"
+  },
+  {
+    "revision": "232361ac06079ab7564fe4211cd45967",
+    "url": "/address-diary-react-client/static/media/property.232361ac.jpg"
+  }
+]);
